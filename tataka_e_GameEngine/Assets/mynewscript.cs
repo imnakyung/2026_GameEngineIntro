859a9f70feae3e6e10806c0975589a71a8fe2911
imnakyung <imnakyung1005@gmail.com>
2026-03-12 15:57:16 +0900

    tataka_e_unity_1

---
+++ tataka_e_GameEngine/Assets/mynewscript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class mynewscript : MonoBehaviour
+{
+    public string playername;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello" + playername); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
