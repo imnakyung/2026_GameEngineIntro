ca2e9343d459ee909811c539c759245003e3be56
imnakyung <imnakyung1005@gmail.com>
2026-03-19 15:30:26 +0900

    add 플레이어 기본 움직임

--- tataka_e_GameEngine/Assets/playercontroller.cs
+++ tataka_e_GameEngine/Assets/playercontroller.cs
@@ -5,11 +5,27 @@ public class playercontroller : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
    
     {
         moveInput = value.Get<Vector2>();
+    }
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)  // 점프 버튼을 누르면
+
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
     void Update()
@@ -22,6 +38,15 @@ public class playercontroller : MonoBehaviour
 
                 transform.localScale = new Vector3(-1, 1, 1);
 
+        if(moveInput.magnitude > 0)
+            myAnimator.SetBool("move", true);
+
+        else
+            myAnimator.SetBool("move", false);
+
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+
+
     }
+
 }
